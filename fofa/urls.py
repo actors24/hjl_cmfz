2de5845f2e89328f1d1c4f0a6dc866893da6c8df
actors <actors1021@163.com>
2020-04-08 16:58:37 +0800

    在视图函数中新增了登录，登陆逻辑以及验证码的获取

--- fofa/urls.py
+++ fofa/urls.py
@@ -4,5 +4,8 @@ from fofa import views
 app_name = 'fofa'
 
 urlpatterns = [
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('get_phone_code/', views.get_phone_code, name='get_phone_code'),
+    path('check_code/', views.check_code, name='check_code'),
 ]
--- fofa/views.py
+++ fofa/views.py
@@ -1,7 +1,51 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from redis import Redis
+from hjl_cmfz.settings import API_KEY
+from utils.send_message import YunPian
+from utils.random_code import get_code
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+red = Redis(host='192.168.133.128', port=6379)
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+@csrf_exempt
+def get_phone_code(request):
+    number = request.POST.get('number')
+    try:
+        result = red.get(number+'_1')
+        if result:
+            print(0)
+            return HttpResponse(0)
+        else:
+            code = get_code()
+            pian = YunPian(API_KEY)
+            if pian.send_message(number, code) == 200:
+                red.set(number+'_1', code, 120)
+                red.set(number+'_2', code, 600)
+                return HttpResponse(1)
+            else:
+                return HttpResponse(0)
+    except:
+        return HttpResponse(0)
+
+
+def check_code(request):
+    number = request.GET.get('mobile')
+    code = request.GET.get('code')
+    try:
+        result = red.get(number+'_2')
+        if result.decode('utf-8') == code:
+            return HttpResponse(1)
+        else:
+            return HttpResponse(0)
+    except:
+        return HttpResponse(0)
