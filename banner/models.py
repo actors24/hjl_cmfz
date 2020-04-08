ec19dace90fd84305040b2fb9ff6a1fc1dcd07c2
actors <actors1021@163.com>
2020-04-08 15:26:35 +0800

    在models.py中新增了一个类

--- banner/models.py
+++ banner/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AutoImage(models.Model):
+    image = models.CharField(max_length=50, blank=True, null=True)
+    image_decoration = models.CharField(max_length=20, blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    upload_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auto_image'
