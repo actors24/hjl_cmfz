bbb3c4b8777a3c56612c173c37723a61e6ee3cf2
actors <actors1021@163.com>
2020-04-08 15:17:14 +0800

    在全局url里配置了banner的路径

--- hjl_cmfz/urls.py
+++ hjl_cmfz/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('fofa/', include('fofa.urls')),
+    path('fofa/', include('banner.urls')),
 ]
