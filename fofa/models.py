9a0e85fd1b6899114e6f49f1b7432baa18864b8f
actors <actors1021@163.com>
2020-04-13 21:21:30 +0800

    移除了部分类

--- fofa/models.py
+++ fofa/models.py
@@ -13,33 +13,6 @@ class Admin(models.Model):
         db_table = 'admin'
 
 
-class AlbumIndex(models.Model):
-    index_name = models.CharField(max_length=20, blank=True, null=True)
-    audio_url = models.CharField(max_length=50, blank=True, null=True)
-    audio_size = models.CharField(max_length=10, blank=True, null=True)
-    album = models.ForeignKey('AlbumList', models.DO_NOTHING, blank=True, null=True)
-    audio_time = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'album_index'
-
-
-class AlbumList(models.Model):
-    album_name = models.CharField(max_length=20, blank=True, null=True)
-    author = models.CharField(max_length=20, blank=True, null=True)
-    speaker = models.CharField(max_length=20, blank=True, null=True)
-    index_num = models.CharField(max_length=5, blank=True, null=True)
-    comment = models.TextField(blank=True, null=True)
-    publish_time = models.DateTimeField(blank=True, null=True)
-    album_image = models.CharField(max_length=50, blank=True, null=True)
-    rate = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'album_list'
-
-
 class CountNum(models.Model):
     count_name = models.CharField(max_length=20, blank=True, null=True)
     read_num = models.CharField(max_length=20, blank=True, null=True)
